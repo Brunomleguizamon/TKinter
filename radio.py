3cdf776eaeb9e830fe8becaf099cf2fca09b8254
Bmleguizamon <brunomleguizamon@gmail.com>
2021-09-06 22:28:15 +0200

    subject: radio buttom

--- radio.py
+++ radio.py
@@ -7,11 +7,24 @@ r = IntVar()
 r.set('2')
 
 
+ESTADOS = [ 
+    ('Feliz','Feliz'),
+    ('Triste','Triste'),
+    ('Amargado','Amargado'),
+    ('Enojado','Enojado')
+        ]
+
+varestado = StringVar()
+varestado.set('Enojado')
+
+for text, estado in ESTADOS:
+    Radiobutton(root, text=text, variable= varestado , value=estado).pack()
+
 # Se elige la variable y el valor que va a asignarle a esta una vez elegido el campo
-Radiobutton(root, text='Option 1', variable=r, value=1).pack()
-Radiobutton(root, text='Option 2', variable=r, value=2).pack()
+#Radiobutton(root, text='Option 1', variable=r, value=1).pack()
+#Radiobutton(root, text='Option 2', variable=r, value=2).pack()
 
 # Muestro la variable r
-Label(root, textvariable=r).pack()
+Label(root, textvariable=varestado).pack()
 
 root.mainloop()
